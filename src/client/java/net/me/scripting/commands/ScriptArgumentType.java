c6c746c903c72f3ad4b1e60e3a849920a3f0cf45
trethore <titou.rethore@gmail.com>
2025-06-23 21:20:24 +0200

    Fix unused variable !

--- src/client/java/net/me/scripting/commands/ScriptArgumentType.java
+++ src/client/java/net/me/scripting/commands/ScriptArgumentType.java
@@ -1,36 +1,33 @@
 package net.me.scripting.commands;
 
 import com.mojang.brigadier.arguments.*;
-import com.mojang.brigadier.context.CommandContext;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 
 import java.util.Arrays;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public enum ScriptArgumentType {
-    WORD("word", StringArgumentType::word, c -> StringArgumentType.getString(c, "arg")),
-    STRING("string", StringArgumentType::string, c -> StringArgumentType.getString(c, "arg")),
-    GREEDY_STRING("greedy", "text", StringArgumentType::greedyString, c -> StringArgumentType.getString(c, "arg")),
-    INTEGER("integer", "int", IntegerArgumentType::integer, c -> IntegerArgumentType.getInteger(c, "arg")),
-    LONG("long", LongArgumentType::longArg, c -> LongArgumentType.getLong(c, "arg")),
-    FLOAT("float", FloatArgumentType::floatArg, c -> FloatArgumentType.getFloat(c, "arg")),
-    DOUBLE("double", DoubleArgumentType::doubleArg, c -> DoubleArgumentType.getDouble(c, "arg")),
-    BOOLEAN("boolean", "bool", BoolArgumentType::bool, c -> BoolArgumentType.getBool(c, "arg"));
+    WORD("word", StringArgumentType::word),
+    STRING("string", StringArgumentType::string),
+    GREEDY_STRING("greedy", "text", StringArgumentType::greedyString),
+    INTEGER("integer", "int", IntegerArgumentType::integer),
+    LONG("long", LongArgumentType::longArg),
+    FLOAT("float", FloatArgumentType::floatArg),
+    DOUBLE("double", DoubleArgumentType::doubleArg),
+    BOOLEAN("boolean", "bool", BoolArgumentType::bool);
 
     private final String[] names;
     private final Supplier<ArgumentType<?>> supplier;
 
 
-    ScriptArgumentType(String name, Supplier<ArgumentType<?>> supplier, Function<CommandContext<FabricClientCommandSource>, Object> getter) {
-        this(new String[]{name}, supplier, getter);
+    ScriptArgumentType(String name, Supplier<ArgumentType<?>> supplier) {
+        this(new String[]{name}, supplier);
     }
 
-    ScriptArgumentType(String name, String alias, Supplier<ArgumentType<?>> supplier, Function<CommandContext<FabricClientCommandSource>, Object> getter) {
-        this(new String[]{name, alias}, supplier, getter);
+    ScriptArgumentType(String name, String alias, Supplier<ArgumentType<?>> supplier) {
+        this(new String[]{name, alias}, supplier);
     }
 
-    ScriptArgumentType(String[] names, Supplier<ArgumentType<?>> supplier, Function<CommandContext<FabricClientCommandSource>, Object> getter) {
+    ScriptArgumentType(String[] names, Supplier<ArgumentType<?>> supplier) {
         this.names = names;
         this.supplier = supplier;
     }
