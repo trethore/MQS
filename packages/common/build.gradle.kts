4d012759386c3a6508c92b37de09f85e3bf844d1
trethore <titou.rethore@gmail.com>
2026-07-20 23:18:14 +0200

    build(common): add GraalJS and Byte Buddy dependencies

--- packages/common/build.gradle.kts
+++ packages/common/build.gradle.kts
@@ -1,9 +1,24 @@
+import io.github.trethore.buildlogic.unpack
+
 plugins {
   `java-library`
   `maven-publish`
 }
 
 dependencies {
+  val graalVersion = providers.gradleProperty("graal_version").get()
+  val byteBuddyVersion = providers.gradleProperty("byte_buddy_version").get()
+
+  // GraalVM dependencies
+  unpack(implementation("org.graalvm.sdk:graal-sdk:$graalVersion"))
+  unpack(implementation("org.graalvm.truffle:truffle-api:$graalVersion"))
+  implementation("org.graalvm.js:js:$graalVersion")
+  unpack(create("org.graalvm.js:js-language:$graalVersion"))
+  unpack(implementation("org.graalvm.js:js-scriptengine:$graalVersion"))
+  // ByteBuddy dependencies
+  unpack(implementation("net.bytebuddy:byte-buddy:$byteBuddyVersion"))
+  unpack(implementation("net.bytebuddy:byte-buddy-agent:$byteBuddyVersion"))
+
   implementation("org.slf4j:slf4j-api:${providers.gradleProperty("common_slf4j_version").get()}")
 }
 
