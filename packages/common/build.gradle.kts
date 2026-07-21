2fe47513db121c33ff068a1899e947fee8ad1b85
trethore <titou.rethore@gmail.com>
2026-07-21 21:28:33 +0200

    fix(build): update dependency versions and improve configuration

--- packages/common/build.gradle.kts
+++ packages/common/build.gradle.kts
@@ -5,22 +5,31 @@ plugins {
   `maven-publish`
 }
 
+configurations.testRuntimeOnly {
+  extendsFrom(configurations.compileOnly.get())
+}
+
 dependencies {
   val graalVersion = providers.gradleProperty("graal_version").get()
   val byteBuddyVersion = providers.gradleProperty("byte_buddy_version").get()
+  val gsonVersion = providers.gradleProperty("gson_version").get()
+  val slf4jApiVersion = providers.gradleProperty("slf4j_api_version").get()
 
-  // GraalVM dependencies
+  // GraalVM
   unpack(implementation("org.graalvm.sdk:graal-sdk:$graalVersion"))
   unpack(implementation("org.graalvm.truffle:truffle-api:$graalVersion"))
   implementation("org.graalvm.js:js:$graalVersion")
-  unpack(create("org.graalvm.js:js-language:$graalVersion"))
   unpack(implementation("org.graalvm.js:js-scriptengine:$graalVersion"))
-  // ByteBuddy dependencies
+  // ByteBuddy
   unpack(implementation("net.bytebuddy:byte-buddy:$byteBuddyVersion"))
   unpack(implementation("net.bytebuddy:byte-buddy-agent:$byteBuddyVersion"))
 
-  implementation("com.google.code.gson:gson:${providers.gradleProperty("gson_version").get()}")
-  implementation("org.slf4j:slf4j-api:${providers.gradleProperty("common_slf4j_version").get()}")
+  // Source
+  unpack(create("org.graalvm.js:js-language:$graalVersion"))
+
+  // GSON & SLF4J provided at runtime
+  compileOnly("com.google.code.gson:gson:$gsonVersion")
+  compileOnly("org.slf4j:slf4j-api:$slf4jApiVersion")
 }
 
 tasks.withType<JavaCompile>().configureEach {
